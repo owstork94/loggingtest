268b3f2c306a738a8ad3416049ee1c291f9f1c75
owstork94 <owstork94@gmail.com>
2023-08-17 13:57:00 +0900

    Initial commit

---
+++ src/main/java/com/sehun/loggingtest/LoggingtestApplication.java
@@ -0,0 +1,13 @@
+package com.sehun.loggingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sehun/loggingtest/LoggingtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehun.loggingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
